aa6a8d3e4aeedd3e8e7893475144a1e6cf843f93
AlexRego <alexandre.rego@aggiamil.usu.edu>
2016-11-05 17:55:22 -0600

    basic functioning of diamond and square

--- terrain.R
+++ terrain.R
@@ -8,19 +8,51 @@ genmat <- function(n){
   return(out.mat)
 }
 
+mat <- genmat(5)
 # create diamond step function
-d.s <- function(n){
-  mat <- genmat(n)
+d.s <- function(mat){
   ## find corners
-  corners <- c(mat[1,1],mat[nrow(mat),1],mat[1,ncol(mat)],mat[nrow(mat),ncol(mat)])
+  ul <- mat[1,1]
+  ur <- mat[1,ncol(mat)]
+  ll <- mat[nrow(mat),1]
+  lr <- mat[nrow(mat),ncol(mat)]
+  corners <- c(ul,ur,ll,lr)
   ## calculate averaged
-  avg <- mean(corners) + rnorm(1,0,5)
+  avg <- mean(corners) + rnorm(1,0,1)
   ## input average into center of matrix
-  mat[ceiling(nrow(mat)/2),ceiling(ncol(mat)/2)] <- avg
+  mat[median(1:nrow(mat)),median(1:ncol(mat))] <- avg
   return(mat)
 }
+mat <- d.s(mat)
 
-## square-step
+# square-step
 s.s <- function(n){
-  mat <- d.s(n)
+  ## find center + corners
+  center <- mat[median(1:nrow(mat)),median(1:ncol(mat))]
+  ul <- mat[1,1]
+  ur <- mat[1,ncol(mat)]
+  ll <- mat[nrow(mat),1]
+  lr <- mat[nrow(mat),ncol(mat)]
+
+  ## placing averages
+  # upper
+  mat[1,median(1:ncol(mat))] <- mean(ul,center,ur) + rnorm(1,0,1)
+  # right
+  mat[median(1:nrow(mat)),ncol(mat)] <- mean(ur,center,lr) + rnorm(1,0,1)
+  # lower
+  mat[nrow(mat),median(1:ncol(mat))] <- mean(ll,center,lr) + rnorm(1,0,1)
+  # left
+  mat[median(1:nrow(mat)),1] <- mean(ul,center,ll) + rnorm(1,0,1)
+  return(mat)
+}
+mat <- s.s(mat)
+# diamond square step
+dss <- function(mat){
+  mat <- mat
+  while(any(is.na(mat))){
+    mat <- d.s(mat)
+    mat <- s.s(mat)
+    mat <- mat
+  }
+  return(mat)
 }
