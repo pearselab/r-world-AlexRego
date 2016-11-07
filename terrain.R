232f35f582d93ea827f3fdd950804ec00500ca58
AlexRego <alexandre.rego@aggiemail.usu.edu>
2016-11-07 09:09:07 -0700

    terrain.R

--- terrain.R
+++ terrain.R
@@ -1,11 +1,12 @@
 # generate matrix with odd dimensions
+mat <-matrix(NA)
 genmat <- function(n){
-  out.mat <- matrix(data=NA, nrow = (2*n)+1, ncol = (2*n)+1)
-  out.mat[1,1] <- rnorm(1,0,10)
-  out.mat[(2*n)+1,1] <- rnorm(1,0,10)
-  out.mat[1,(2*n)+1] <- rnorm(1,0,10)
-  out.mat[(2*n)+1,(2*n)+1] <- rnorm(1,0,10)
-  return(out.mat)
+  mat <- matrix(data=NA, nrow = (2*n)+1, ncol = (2*n)+1)
+  mat[1,1] <- rnorm(1,0,10)
+  mat[(2*n)+1,1] <- rnorm(1,0,10)
+  mat[1,(2*n)+1] <- rnorm(1,0,10)
+  mat[(2*n)+1,(2*n)+1] <- rnorm(1,0,10)
+  return(mat)
 }
 
 # create diamond step function
@@ -23,4 +24,7 @@ d.s <- function(n){
 ## square-step
 s.s <- function(n){
   mat <- d.s(n)
+  middle <- mat[ceiling(nrow(mat)/2),ceiling(ncol(mat)/2)]
+  ## Averages of points
+  mat[1,ceiling(ncol(mat)/2)] <- mean(c(middle,mat[1,1],mat[nrow(mat),1]))
 }
