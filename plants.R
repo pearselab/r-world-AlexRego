6df4adfb3752ea91e4402b6ffa10c68b1a3d110a
AlexRego <alexandre.rego@aggiamil.usu.edu>
2016-11-18 15:33:38 -0700

    rpackaging

--- plants.R
+++ plants.R
@@ -1,3 +1,21 @@
+#setting up parameters
+names <- c('lupin', 'alfalfa')
+repro <- c(.5,.5)
+survive <- c(.9,.9)
+comp.mat <- matrix(1,2,2)
+comp.mat[1,2] <- .7
+comp.mat[2,1] <- .3
+rownames(comp.mat) <- names
+colnames(comp.mat) <- names
+
+#' Setting up plant information
+#'
+#' @param repro
+#' @param survive
+#' @param comp.mat
+#' @param names
+#' @return returns a list containing all the parameters that will constitute the ecosystem
+
 #plant set up
 setup.plants <- function(repro,survive,comp.mat, names=NULL){
   if(is.null(names)){
@@ -10,17 +28,24 @@ setup.plants <- function(repro,survive,comp.mat, names=NULL){
   survive <- setNames(survive,names)
   return(list(repro=repro,survive=survive,comp.mat=comp.mat,names=names))
 }
-#setting up parameters
-names <- c('lupin', 'alfalfa')
-repro <- c(.5,.5)
-survive <- c(.9,.9)
-comp.mat <- matrix(1,2,2)
-comp.mat[1,2] <- .7
-comp.mat[2,1] <- .3
-rownames(comp.mat) <- names
-colnames(comp.mat) <- names
 
 info <- setup.plants(repro,survive, comp.mat,names)
+
+# set up ecosystem
+eco <- array('',dim=c(dim(terrain),3))
+
+for(i in 1:length(terrain))
+  eco[sample(nrow(eco),1), sample(ncol(eco),1), 1] <- sample(info$names,1)
+
+for(i in seq_len(dim(eco)[3])){
+  eco[,,i][is.na(terrain)] <- NA
+}
+
+#' Survival function of plants in ecosystem
+#'
+#' @param cell This is a particular cell in the ecosystem at a particular time step
+#' @param info Info is a list contianing all of the parameter information of the ecosystem
+#' @return returns the cell to be input back into the larger ecosystem matrix
 
 # survival function
 survive <- function(cell,info){
@@ -40,46 +65,31 @@ survive <- function(cell,info){
   return(cell)
 }
 
-#timestep for plant
-plant.timestep <- function(eco,info){
-  for(k in 1:(dim(eco)[3]-1)){
-    for(i in 1:dim(eco)[1]){
-      for(j in 1:dim(eco)[2]){
-        eco[i,j,k+1] <- survive(eco[i,j,k], info)
-        reproduce(i,j,k,eco,info)
-      }
-    }
-  }
-  return(eco)
-}
-
-# set up ecosystem
-eco <- array('',dim=c(dim(terrain),3))
-
-for(i in 1:length(terrain))
-  eco[sample(nrow(eco),1), sample(ncol(eco),1), 1] <- sample(info$names,1)
-
-for(i in seq_len(dim(eco)[3])){
-  eco[,,i][is.na(terrain)] <- NA
-}
-
-# run plant ecosystem
-# run.eco <- function(eco=eco, info=info,timesteps)
-# for(i in 1:timesteps){
-#   plant.timestep(ecosystem, info)
-# }
-
+#' reproduction function of plants in ecosystem
+#'
+#' @param row The row position of the ecosystem array to be input
+#' @param col The column position of the ecosystem array to be input
+#' @param time Which timestep in the array that will be input
+#' @param eco The ecosystem array
+#' @param info Info is a list contianing all of the parameter information of the ecosystem
+#' @return returns the ecosystem array, now with plants having reproduced and undergone competition from surrounding cells
 #reproduction
 #row and col are going to be position for certain iteration
 reproduce <- function(row,col,time, eco,info){
-  possible.locations <- as.matrix(expand.grid(row+c(-1,0,1),col+c(-1,0,1)))
-  possible.locations[possible.locations < 1] <- dim(eco)[1]
-  possible.locations[possible.locations > dim(eco)[1]] <- 1
-  for(r in 1:nrow(possible.locations)){
+  surrounding <- as.matrix(expand.grid(row+c(-1,0,1),col+c(-1,0,1)))
+  surrounding[surrounding < 1] <- dim(eco)[1]
+  surrounding[surrounding > dim(eco)[1]] <- 1
+  for(r in 1:nrow(surrounding)){
     if(eco[row,col,time] == "alfalfa" | eco[row,col,time] == "lupin"){
       if(runif(1) <= info$repro[eco[row,col,time]]){
-        if(!is.na(eco[possible.locations[r,1],possible.locations[r,2],time])){
-          eco[possible.locations[r,1],possible.locations[r,2],time] <- eco[row,col,time]
+        if(!is.na(eco[surrounding[r,1],surrounding[r,2],time])){
+          if(eco[surrounding[r,1],surrounding[r,2],time] == "lupin" | eco[surrounding[r,1],surrounding[r,2],time] == "alfalfa"){
+            if(runif(1) <= info$comp.mat[eco[row,col,time],eco[surrounding[r,1],surrounding[r,2],time]]){
+              eco[surrounding[r,1],surrounding[r,2],time] <- eco[row,col,time]
+            }
+          } else{
+              eco[surrounding[r,1],surrounding[r,2],time] <- eco[row,col,time]
+            }
         }
       }
     }
@@ -87,7 +97,21 @@ reproduce <- function(row,col,time, eco,info){
   return(eco)
 }
 
-# competition
-fight <- function(species_names,info){
-  sample(species_names, 1, prob=comp.mat[row,column])
+#' Timestep function of plants in ecosystem
+#'
+#' @param eco The ecosystem array containing information on plant position at each time step
+#' @param info Info is a list contianing all of the parameter information of the ecosystem
+#' @return returns the ecosystem array, now with plants at all timesteps having reproduced in adjacent cells
+
+#timestep for plant
+plant.timestep <- function(eco,info){
+  for(k in 1:(dim(eco)[3]-1)){
+    for(i in 1:dim(eco)[1]){
+      for(j in 1:dim(eco)[2]){
+        eco[i,j,k+1] <- survive(eco[i,j,k], info)
+        eco <- reproduce(i,j,k,eco,info)
+      }
+    }
+  }
+  return(eco)
 }
--- terrain.R
+++ terrain.R
@@ -1,4 +1,13 @@
 # generate matrix with odd dimensions
+
+
+#' Generates starting terrain matrix; includes starting values
+#'
+#' @param n Size of the matrix will be (2^n)+1, which will always produce a
+#'   matrix that is subdivisible into matrices with odd sides
+#' @return a terrain matrix
+
+
 genmat <- function(n){
   side <- (2^n)+1
   mat <- matrix(data=NA, nrow = side, ncol = side)
@@ -9,7 +18,12 @@ genmat <- function(n){
   return(mat)
 }
 
-mat <- genmat(3)
+#' The diamond-step algorithm
+#'
+#' @param mat This inputs the matrix on which the diamond-step algorithm will act upon
+#' @param sd This is the noise that will be added to each iteration of the DS algorithm
+#' @return returns the matrix, now manipulated to have a center
+
 # create diamond step function
 d.s <- function(mat, sd){
   ## find corners
@@ -25,8 +39,15 @@ d.s <- function(mat, sd){
   return(mat)
 }
 
+#' The square-step algorithm
+#'
+#' @param mat This inputs the matrix on which the square-step algorithm will act upon
+#' @param sd This is the noise that will be added to each iteration of the SS algorithm
+#' @return returns the matrix, now manipulated to contain north/east/south/west points
+#'    originating from the center produced by the diamond-step
+
 # square-step
-s.s <- function(mat, var){
+s.s <- function(mat, sd){
   middle <- mat[ceiling(nrow(mat)/2),ceiling(ncol(mat)/2)]
   ## Averages of points
   mat[1,ceiling(ncol(mat)/2)] <- mean(c(middle,mat[1,1],mat[nrow(mat),1]))
@@ -48,11 +69,19 @@ s.s <- function(mat, var){
   return(mat)
 }
 
+#' The diamond-square-step algorithm
+#'
+#' @param n Will create the matrix
+#' @param sd This is the noise that will be added to each iteration of the DSS algorithm
+#' @return returns the matrix, now manipulated by the DSS algorithm
+
 # diamond square step
 dss <- function(n, sd){
   mat <- genmat(n)
   side <- (2^n)+1
   sd <- sd
+  ## I talked with Bodie and Spencer about how to implement the DSS on sub-matrices.
+  ## k will run through half-sides of each sub-matrix within the original matrix
   for(k in 2^(n:1)){
     for(i in seq(1,side-1,by=k)){
       for(j in seq(1,side-1,by=k)){
@@ -65,8 +94,15 @@ dss <- function(n, sd){
   return(mat)
 }
 
+#' The wrapper function for the diamond-square-step algorithm
+#'
+#' @param n Generates original matrix with side lengths (2^n)+1
+#' @param sd This is the noise that will be added to each iteration of the DSS algorithm
+#' @param lakes If True, will induce values in the matrix less than 0 to be NA values, indicating water
+#' @return returns the completed matrix
+
 # wrapper function
-terrain <- function(n, sd, lakes){
+terrain.gen <- function(n, sd, lakes){
   mat <- dss(n, sd)
   if(lakes==TRUE){
     mat[mat<0] <- NA
